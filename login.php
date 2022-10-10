fbc11bf1c14d468911d0f9d691fbff0f8d6a66db
Taris Henrique <taris.tj@hotmail.com>
2022-10-10 11:32:28 -0300

    42m12s - Database

---
+++ login.php
@@ -0,0 +1,26 @@
+<?php include 'header.php' ?>
+
+<div class="row">
+    <div class="col-md-6 offset-md-3">
+        <h2>Login</h2>
+
+        <form action="login.salvar.php" method="POST">
+
+            <div>
+                <input type="text" class="form-control" placeholder="Usuário ou Email" name="txtLogin">
+            </div>
+
+            <div>
+                <input type="password" class="form-control" placeholder="Digite a senha" name="txtSenha">
+            </div>
+
+            <br>
+
+            <button type="submit" name="botaoSubmit" class="btn btn-primary">Login</button>
+        </form>
+
+    </div>
+</div>
+
+
+<?php include 'footer.php'  ?>
--- signup.php
+++ signup.php
@@ -1,22 +1,29 @@
 <?php include 'header.php' ?>
 
 <div class="row">
-    <div class="col">
+    <div class="col-md-6 offset-md-3">
         <h2>Cadastrar usuário</h2>
 
-        <form action="signup.inc.php" method="POST">
+        <form action="signup.salvar.php" method="POST">
 
-            <div class="col-lg-3">
-                <label class="form-label">Usuário:</label>
-                <input type="text" class="form-control" id="usuario" placeholder="Digite seu login" name="txtUsuario">
+            <div>
+                <input type="text" class="form-control" placeholder="Nome Completo:" name="txtNome">
             </div>
-            <div class="col-lg-3">
-                <label class="form-label">Senha:</label>
-                <input type="password" class="form-control" id="senha" placeholder="Digite a senha" name="txtSenha">
+            <div>
+                <input type="text" class="form-control" placeholder="Email:" name="txtEmail">
+            </div>
+            <div>
+                <input type="text" class="form-control" placeholder="Usuário" name="txtUid">
+            </div>
+            <div>
+                <input type="password" class="form-control" placeholder="Digite a senha" name="txtSenha">
+            </div>
+            <div>
+                <input type="password" class="form-control" placeholder="Repita a senha" name="txtSenhaRepete">
             </div>
             <br>
 
-            <button type="submit" class="btn btn-primary">Enviar</button>
+            <button type="submit" name="botaoSubmit" class="btn btn-primary">Enviar</button>
         </form>
 
     </div>
